3a8db49b59dbcbf394d445407bd37446fd26a22a
joseluiscunha18-lang <joseluiscunha18@gmail.com>
2026-06-30 01:45:15 +0200

    Delete app/checkout/[storeSlug] directory

--- app/checkout/[storeSlug]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { StoreCheckoutClient } from './StoreCheckoutClient';
-
-export default async function StoreCheckoutPage({ params }: { params: Promise<{ storeSlug: string }> }) {
-  const { storeSlug } = await params;
-  return <StoreCheckoutClient storeSlug={storeSlug} />;
-}
