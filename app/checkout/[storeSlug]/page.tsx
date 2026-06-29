33fa29b9465e907c8dfa4b132642188e0c561692
joseluiscunha18-lang <joseluiscunha18@gmail.com>
2026-06-29 22:55:13 +0200

    Add files via upload

--- app/checkout/[storeSlug]/page.tsx
+++ app/checkout/[storeSlug]/page.tsx
@@ -1,1 +1,5 @@
+import { StoreCheckoutClient } from './StoreCheckoutClient';
 
+export default function StoreCheckoutPage({ params }: { params: { storeSlug: string } }) {
+  return <StoreCheckoutClient storeSlug={params.storeSlug} />;
+}
